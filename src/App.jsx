9a8ceb6ccc8fd2b8548f7dab835b5d57b5fe4129
Shahbaz7307 <shahbazshabbir64@gmail.com>
2023-12-21 19:09:55 +0500

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contact from "./components/Contact";
 import ScrollToTop from "./components/ScrollTop";
 
 export default function App() {
-  const a = 2;
+  const a = 21;
   return (
     <div className="flex">
       <div>
